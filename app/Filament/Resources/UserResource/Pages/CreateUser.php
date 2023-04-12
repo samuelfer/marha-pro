14b62fed71aa1695c30775f4dfe5cc257d7858cc
sfsantos <sfsantos@tce.pb.gov.br>
2023-04-12 19:32:00 -0300

    Remove slashe in the method redirect

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -12,6 +12,6 @@ class CreateUser extends CreateRecord
 
     protected function getRedirectUrl(): string
     {
-        return $this->getResource()::getUrl('/index');
+        return $this->getResource()::getUrl('index');
     }
 }
